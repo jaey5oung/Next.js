e34115f8ec733534b8398c74834fe0653b36f714
jaey5oung <jaey5oung@naver.com>
2022-08-10 18:34:04 +0900

    ✨ feat : data-fns remark 패키지 적용 및 getStaticPaths 사용

--- pages/posts/[id].js
+++ pages/posts/[id].js
@@ -1,16 +1,22 @@
+import Date from "../../components/Date"
 import Layout from "../../components/Layout"
+import utilStyles from "../../styles/utils.module.css"
 
 import { getAllPostIds, getPostData } from "../../lib/posts"
+import Head from "next/head"
+import { useRouter } from "next/router"
 
 export async function getStaticPaths() {
   const paths = getAllPostIds()
   return {
     paths,
     fallback: false,
+    // false 없는 링크로 갔을때 404를 떨굼
   }
 }
 export async function getStaticProps({ params }) {
-  const postData = getPostData(params.id)
+  console.log(params)
+  const postData = await getPostData(params.id)
   return {
     props: {
       postData,
@@ -19,13 +25,22 @@ export async function getStaticProps({ params }) {
 }
 
 export default function Post({ postData }) {
+  const router = useRouter()
+  if (router.isFallback) {
+    return <div>loading...</div>
+  }
   return (
     <Layout>
-      {postData.title}
-      <br />
-      {postData.id}
-      <br />
-      {postData.date}
+      <Head>
+        <title>{postData.title}</title>
+      </Head>
+      <article>
+        <h1 className={utilStyles.headingXl}>{postData.title}</h1>
+        <div className={utilStyles.lightText}>
+          <Date dateString={postData.date} />
+        </div>
+        <div dangerouslySetInnerHTML={{ __html: postData.contentHtml }} />
+      </article>
     </Layout>
   )
 }
